cef7258371e8b49fa47bb9c0e5add5b7f0d46a05
Wiktor Rup <wiktor-rup@wp.pl>
2019-01-09 22:36:44 +0100

    JPQL functions

--- src/main/java/my/project/Main.java
+++ src/main/java/my/project/Main.java
@@ -5,9 +5,7 @@ import my.project.domain.Employee;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.Query;
 
 public class Main {
 
@@ -20,24 +18,25 @@ public class Main {
 
         addEmployees();
 
-        TypedQuery<Employee> query = entityManager.createQuery("select e from Pracownik e where e.lastName in :names", Employee.class);
 
-        List<String> names= new ArrayList<String>();
-        names.add("Mateusiak");
-        names.add("Bednarek");
+        Query query = entityManager.createQuery("select substring(e.firstName, 1, 3), trim(e.lastName), lower(e.firstName), upper(e.firstName), length(e.firstName) from Pracownik e where e.firstName='Karol'");
 
-        query.setParameter("names", names);
+//        Query query = entityManager.createQuery("select avg(e.salary), min(e.salary), max(e.salary), sum(e.salary), count(e) from Pracownik e");
+//
+        Object [] result = (Object[]) query.getSingleResult();
+        System.out.println("Pierwsze litery imienia: " + result[0]);
+        System.out.println("Naziwsko: |" + result[1] + "|");
+        System.out.println("Imie malymi literami: " + result[2]);
+        System.out.println("Imie wielkimi literami: " + result[3]);
+        System.out.println("Dlugosc imienia: " + result[4]);
 
-        for (Employee employee: query.getResultList()){
-            System.out.println(employee);
-        }
 
         entityManager.close();
         entityManagerFactory.close();
     }
 
     private static void addEmployees() {
-        addEmployee("Karol", "Mateusiak", 2633);
+        addEmployee("Karol", "     Mateusiak     ", 2633);
         addEmployee("Marika", "Bednarek", 2345);
         addEmployee("Jan", "Mateusiak", 7346);
         addEmployee("Daria", "Kowalska", 2352);
